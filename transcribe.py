dabc9319866c0551bf07b6936743a4ded62211e6
James Spencer <Merfi.Dev@gmail.com>
2026-05-08 14:37:53 -0600

    transcribe timing feature

--- transcribe.py
+++ transcribe.py
@@ -14,6 +14,8 @@ currentTxt = TRANSCRIPTS / current.with_suffix(".txt").name
 
 model = WhisperModel("base", device = "cpu", compute_type = "int8")
 
+start_time = time.perf_counter()
+
 segments, info = model.transcribe(current, beam_size = 5)
 
 with open(currentTxt, "w", encoding="utf-8") as f:
