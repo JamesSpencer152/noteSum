297e8a413dddcfe031a5a6eac818d7c6dabb41c6
James Spencer <Merfi.Dev@gmail.com>
2026-05-08 14:53:48 -0600

    transcribe added to main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import fileFunctions
+from transcribe import transcribe
 
 print("Audio pulled", flush = True)
 fileFunctions.pullFiles()
 
 print("Audio sorted", flush = True)
 fileFunctions.sortAudio()
+
+print("Audio transcribed", flush = True)
+transcribe()
--- transcribe.py
+++ transcribe.py
@@ -3,32 +3,33 @@ from pathlib import Path
 from fileFunctions import moveFile
 import time
 
-start_time = time.perf_counter()
+def transcribe():
+    start_time = time.perf_counter()
 
-PROCESSING = Path("audio/processing")
-TRANSCRIPTS = Path("audio/transcripts")
-PROCESSED = Path("audio/processed")
+    PROCESSING = Path("audio/processing")
+    TRANSCRIPTS = Path("audio/transcripts")
+    PROCESSED = Path("audio/processed")
 
-files = [f for f in PROCESSING.iterdir() if f.suffix == ".m4a"]
-current = files[0]
+    files = [f for f in PROCESSING.iterdir() if f.suffix == ".m4a"]
+    current = files[0]
 
-currentTxt = TRANSCRIPTS / current.with_suffix(".txt").name
+    currentTxt = TRANSCRIPTS / current.with_suffix(".txt").name
 
-model = WhisperModel("base", device = "cpu", compute_type = "int8")
+    model = WhisperModel("base", device = "cpu", compute_type = "int8")
 
-start_time = time.perf_counter()
+    start_time = time.perf_counter()
 
-segments, info = model.transcribe(current, beam_size = 5)
+    segments, info = model.transcribe(current, beam_size = 5)
 
-with open(currentTxt, "w", encoding="utf-8") as f:
-    for segment in segments:
-        f.write(segment.text + "\n")
-        print(f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}")
+    with open(currentTxt, "w", encoding="utf-8") as f:
+        for segment in segments:
+            f.write(segment.text + "\n")
+            print(f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}")
 
-print("transcription saved to ", currentTxt)
+    print("transcription saved to ", currentTxt)
 
-moveFile(current, PROCESSED)
+    moveFile(current, PROCESSED)
 
-end_time = time.perf_counter()
+    end_time = time.perf_counter()
 
-print(f"Elapsed time: {end_time - start_time:.4f} seconds")
+    print(f"Elapsed time: {end_time - start_time:.4f} seconds")
