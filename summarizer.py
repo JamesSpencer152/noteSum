6805762fd924325d10573749d8ec37551a98f38a
James Spencer <Merfi.Dev@gmail.com>
2026-05-08 23:30:46 -0600

    summarizer bug fixes

--- summarizer.py
+++ summarizer.py
@@ -16,21 +16,27 @@ def summarize():
     sumText = SUMMARIES / f"{transcriptPath.stem} SUM.txt"
 
     start_time = time.perf_counter()
+    print("Begin Summary", flush = True)
 
     with open(transcriptPath, "r") as file:
         transcriptText = file.read()
 
     summary = ollama.generate(
         model = "llama3.2:3b",
-        prompt = PROMPT_TEMPLATE.format(transcript = transcriptText)
+        prompt = PROMPT_TEMPLATE.format(transcript = transcriptText),
+        stream = True
     )
 
     with open(sumText, "w", encoding="utf-8") as f:
-        f.write(summary["response"])
+        for chunk in summary:
+            text = chunk["response"]
+
+            print(text, end="", flush=True)
+            f.write(text)
 
     end_time = time.perf_counter()
 
-    print("Summary saved to: " + sumText)
+    print("Summary saved to: " + sumText, flush = True)
 
     print(f"Elapsed time: {end_time - start_time:.4f} seconds")
 
